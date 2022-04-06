4361418536d0ca2392ac0e7bfe47cf6e481329b0
Bharat Kathi <bharat1031@gmail.com>
2022-04-06 01:01:38 -0700

    Initial commit

---
+++ lab1_bk1031/helloWorld.cpp
@@ -0,0 +1,10 @@
+// helloWorld.cpp  for UCSB CS32 S22
+// Edited by: Bharat Kathi
+// minimal Hello World! program for testing Makefiles
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ lab1_bk1031/lab1Test.h
@@ -0,0 +1,14 @@
+//
+// Created by Bharat Kathi on 4/5/22.
+//
+
+#ifndef LAB1_BK1031_LAB1TEST_H
+#define LAB1_BK1031_LAB1TEST_H
+
+
+class lab1Test {
+
+};
+
+
+#endif //LAB1_BK1031_LAB1TEST_H
