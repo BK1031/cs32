9b0f439e660074bb4f0cba349ec74d39aea3fc40
Bharat Kathi <bharat1031@gmail.com>
2022-04-26 19:32:05 -0700

    Completed student.cpp lab4

--- lab4/Student.cpp
+++ lab4/Student.cpp
@@ -2,10 +2,10 @@
 #include <sstream>
 #include <string>
 
-Student::Student(int perm, 
-		 std::string lastName, 
-		 std::string firstAndMiddleNames) {
-  // stub
+Student::Student(int perm, std::string lastName, std::string firstAndMiddleNames) {
+    this->perm = perm;
+    this->lastName = lastName;
+    this->firstAndMiddleNames = firstAndMiddleNames;
 }
 
 // construct a Student object from a single line of
@@ -13,47 +13,46 @@ Student::Student(int perm,
 
 Student::Student (std::string csvString) {
 
-  std::istringstream iss(csvString);
+    std::istringstream iss(csvString);
 
-  // This version of getline takes an input string (istream), a string, 
-  // and a delimiter character (in this case, comma).
-  // We read from the stream until the delimiter is encountered 
-  
-  std::string permAsString;
-  getline(iss, permAsString, ',');
-  this->perm = std::stoi(permAsString); // stoi converts std::string to 
-int
+    // This version of getline takes an input string (istream), a string,
+    // and a delimiter character (in this case, comma).
+    // We read from the stream until the delimiter is encountered
 
-  getline(iss, this->lastName, ',');
+    std::string permAsString;
+    getline(iss, permAsString, ',');
+    this->perm = std::stoi(permAsString); // stoi converts std::string to int
 
-  getline(iss, this->firstAndMiddleNames, '\n');
+    getline(iss, this->lastName, ',');
+
+    getline(iss, this->firstAndMiddleNames, '\n');
 }
 
 
-int Student::getPerm() const { 
-  return -999; // stub
+int Student::getPerm() const {
+    return perm;
 }
 
-std::string Student::getLastName() const { 
-  return std::string("Stuberson");
+std::string Student::getLastName() const {
+    return lastName;
 }
 
 std::string Student::getFirstAndMiddleNames() const {
-   return std::string("Stub Toe");
+    return firstAndMiddleNames;
 }
 
-std::string Student::getFullName() const { 
-  return std::string("Stub Toe Stuberson");
+std::string Student::getFullName() const {
+    return firstAndMiddleNames + " " + lastName;
 }
 
-std::string Student::toString() const { 
-  // e.g. [12345,Smith,Malory Logan]
-  std::ostringstream oss;
-  
-  oss << "[" 
+std::string Student::toString() const {
+    // e.g. [12345,Smith,Malory Logan]
+    std::ostringstream oss;
+
+    oss << "["
       << this->getPerm() << ","
       << this->getLastName() << ","
       << this->getFirstAndMiddleNames() << "]";
-  return oss.str();
+    return oss.str();
 } 
 
